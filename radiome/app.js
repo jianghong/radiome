f307030136281ed501b1688e1f5baa8e189d59fb
Jackson Hong <jackson.hong@kontagent.com>
2013-05-18 21:32:35 -0400

    s

--- radiome/app.js
+++ radiome/app.js
@@ -7,18 +7,26 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , passport = require('passport')
+  , User = require('./routes/user').User
+  , LocalStrategy = require('passport-local').Strategy
+  , passwordHash = require('password-hash');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,8 +35,42 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!passwordHash.verify(password, user.password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      console.log(user);
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    console.log(user);
+    done(err, user);
+  });
+});
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/signup', user.signup);
+app.post('/addUser', user.addUser);
+app.get('/login', user.login);
+app.post('/login',
+	passport.authenticate('local', {successRedirect: '/',
+									failureRedirect: '/login'
+									}));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- radiome/routes/user.js
+++ radiome/routes/user.js
@@ -1,8 +1,61 @@
+var mongoose = require('mongoose'),
+	passport = require('passport'),
+	passwordHash = require('password-hash'),
+	fs = require('fs');
 
+
+mongoose.connect('mongodb://localhost/test');
+
+function encodePassword(pass) {
+	var hashedPassword = passwordHash.generate(pass);
+	return hashedPassword;
+}
+
+var userSchema = mongoose.Schema({
+	username: {type: String, required: true, unique: true, trim: true, lowercase: true},
+	password: {type: String, required: true}
+});
+
+var User = mongoose.model('User', userSchema);
+
+exports.User = User;
 /*
  * GET users listing.
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+	User.find({}, function (err, data) {
+		if (err) res.send("user not found");
+		res.json(data);
+	});
 };
+
+exports.signup = function(req, res) {
+	res.render('signup', {title: "radiome"});
+};
+
+exports.addUser = function(req, res){
+	var newUser = new User({
+		username: req.body.username,
+		password: encodePassword(req.body.password)
+	});
+
+	newUser.save( function (err, data) {
+		if (err) res.send(500, err);
+		else {
+			req.login(newUser, function (err) {
+				if (err) res.send('FAIL');
+				res.redirect('/stream');
+			});
+		}
+	});
+};
+
+exports.login = function (req, res) {
+	res.render('login', {title: "radiome"});
+};
+
+exports.stream = function (req, res) {
+	res.render('stream', {user: req.user});
+};
+
